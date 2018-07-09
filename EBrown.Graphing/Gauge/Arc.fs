ec2f4b3a4fb003e46b41a2dffeb0ed7e6468b422
EBrown8534 <EBrown8534@gmail.com>
2018-07-09 08:35:52 -0400

    Rename `min` and `max` parameters to `lower` and `upper`, respectively.

--- EBrown.Graphing/Gauge/Arc.fs
+++ EBrown.Graphing/Gauge/Arc.fs
@@ -7,14 +7,14 @@ open System
 
 let defaultConfig = Default
 
-let generate<'a> configuration (toFloat : 'a -> float32) (formatter : 'a -> string) (max : 'a) (min : 'a) (value : 'a) =
+let generate<'a> configuration (toFloat : 'a -> float32) (formatter : 'a -> string) (upper : 'a) (lower : 'a) (value : 'a) =
     use defaultFont = new Font("Arial", 12.f, FontStyle.Regular)
     let font = configuration.Font |> Option.defaultValue defaultFont
 
     let imageWidth = configuration.Width - configuration.Padding.Top - configuration.Padding.Bottom
     let imageHeight = configuration.Height - configuration.Padding.Left - configuration.Padding.Right
     let gaugeSweep = 180.0f
-    let angle = gaugeSweep * (((value |> toFloat) - (min |> toFloat)) / ((max |> toFloat) - (min |> toFloat)))
+    let angle = gaugeSweep * (((value |> toFloat) - (lower |> toFloat)) / ((upper |> toFloat) - (lower |> toFloat)))
     let startAngle = 180.0f + (180.0f - gaugeSweep) / 2.0f
 
     let image = new Bitmap(imageWidth + configuration.Padding.Left + configuration.Padding.Right, imageHeight + configuration.Padding.Top + configuration.Padding.Bottom)
@@ -50,9 +50,9 @@ let generate<'a> configuration (toFloat : 'a -> float32) (formatter : 'a -> stri
     let gaugeLabelOffsetX = (configuration.Padding.Left |> float32) + (configuration.GaugeWidth / 2 |> float32)
     let imageMidX = (imageWidth |> float32) * 0.5f + (configuration.Padding.Left |> float32)
     drawLabel (value |> formatter) (PointF(imageMidX, (imageWidth |> float32) * 0.5f + (configuration.Padding.Top |> float32) - 10.f))
-    drawLabel (min |> formatter) (PointF(gaugeLabelOffsetX, (imageWidth |> float32) * 0.5f + 5.f + (configuration.Padding.Top |> float32)))
-    drawLabel (max |> formatter) (PointF((image.Width |> float32) - gaugeLabelOffsetX, (imageWidth |> float32) * 0.5f + 5.f + (configuration.Padding.Top |> float32)))
+    drawLabel (lower |> formatter) (PointF(gaugeLabelOffsetX, (imageWidth |> float32) * 0.5f + 5.f + (configuration.Padding.Top |> float32)))
+    drawLabel (upper |> formatter) (PointF((image.Width |> float32) - gaugeLabelOffsetX, (imageWidth |> float32) * 0.5f + 5.f + (configuration.Padding.Top |> float32)))
     image
     
-let generateDelegate<'a> configuration (toFloat : Func<'a, float32>) (formatter : Func<'a, string>) (max : 'a) (min : 'a) (value : 'a) =
-    generate configuration toFloat.Invoke formatter.Invoke max min value
+let generateDelegate<'a> configuration (toFloat : Func<'a, float32>) (formatter : Func<'a, string>) (upper : 'a) (lower : 'a) (value : 'a) =
+    generate configuration toFloat.Invoke formatter.Invoke upper lower value
--- EBrown.Graphing/Gauge/Linear.fs
+++ EBrown.Graphing/Gauge/Linear.fs
@@ -7,13 +7,13 @@ open System
 
 let defaultConfig = { Default with GaugeWidth = 32; Height = 50 }
 
-let generate<'a> config (toFloat : 'a -> float32) (formatter : 'a -> string) (max : 'a) (min : 'a) (value : 'a) =
+let generate<'a> config (toFloat : 'a -> float32) (formatter : 'a -> string) (upper : 'a) (lower : 'a) (value : 'a) =
     use defaultFont = new Font("Arial", 12.f, FontStyle.Regular)
     let font = config.Font |> Option.defaultValue defaultFont
 
     let imageWidth = config.Width - config.Padding.Top - config.Padding.Bottom
     let imageHeight = config.Height - config.Padding.Left - config.Padding.Right
-    let length = ((imageWidth |> float32) * (((value |> toFloat) - (min |> toFloat)) / ((max |> toFloat) - (min |> toFloat)))) |> int
+    let length = ((imageWidth |> float32) * (((value |> toFloat) - (lower |> toFloat)) / ((upper |> toFloat) - (lower |> toFloat)))) |> int
 
     let image = new Bitmap(imageWidth + config.Padding.Left + config.Padding.Right, imageHeight + config.Padding.Top + config.Padding.Bottom)
     use g = Graphics.FromImage(image)
@@ -41,9 +41,9 @@ let generate<'a> config (toFloat : 'a -> float32) (formatter : 'a -> string) (ma
     let gaugeLabelOffsetX = (config.Padding.Left |> float32)
     let imageMidX = (imageWidth |> float32) * 0.5f + (config.Padding.Left |> float32)
     drawLabelCentered (value |> formatter) (PointF(imageMidX, (config.Padding.Top |> float32) + (config.GaugeWidth |> float32) + 5.f))
-    drawLabel (min |> formatter) (PointF(gaugeLabelOffsetX, (config.Padding.Top |> float32) + (config.GaugeWidth |> float32) + 5.f))
-    drawLabel (max |> formatter) (PointF((image.Width |> float32) - gaugeLabelOffsetX - g.MeasureString(max |> formatter, font).Width, (config.Padding.Top |> float32) + (config.GaugeWidth |> float32) + 5.f))
+    drawLabel (lower |> formatter) (PointF(gaugeLabelOffsetX, (config.Padding.Top |> float32) + (config.GaugeWidth |> float32) + 5.f))
+    drawLabel (upper |> formatter) (PointF((image.Width |> float32) - gaugeLabelOffsetX - g.MeasureString(upper |> formatter, font).Width, (config.Padding.Top |> float32) + (config.GaugeWidth |> float32) + 5.f))
     image
     
-let generateDelegate<'a> configuration (toFloat : Func<'a, float32>) (formatter : Func<'a, string>) (max : 'a) (min : 'a) (value : 'a) =
-    generate configuration toFloat.Invoke formatter.Invoke max min value
+let generateDelegate<'a> configuration (toFloat : Func<'a, float32>) (formatter : Func<'a, string>) (upper : 'a) (lower : 'a) (value : 'a) =
+    generate configuration toFloat.Invoke formatter.Invoke upper lower value
